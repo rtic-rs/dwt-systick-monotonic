73c459ec5351d8dfb88698a5ba3336965b66a328
Emil Fresk <emil.fresk@gmail.com>
2021-03-25 16:17:35 +0100

    Move to const generics

--- src/lib.rs
+++ src/lib.rs
@@ -2,42 +2,27 @@
 
 #![no_std]
 
-use core::marker::PhantomData;
 use cortex_m::peripheral::{syst::SystClkSource, DCB, DWT, SYST};
 use rtic_monotonic::{
     embedded_time::{clock::Error, fraction::Fraction},
     Clock, Instant, Monotonic,
 };
-pub use typenum::consts;
-use typenum::Unsigned;
 
 /// DWT and Systick combination implementing `embedded_time::Clock` and `rtic_monotonic::Monotonic`
 ///
-/// As `typenum` only has continuous numbers up to about 1000, this implementation is split into 3
-/// generic parts, MHz, KHz and Hz, where each can be in the span [0, 1000]. The final speed of
-/// this monotonic will be `MHz * 1_000_000 + KHz * 1_000 + Hz`.
-pub struct DwtSystick<MHZ, KHZ, HZ> {
+/// The frequency of the DWT and SysTick is encoded using the parameter `FREQ`.
+pub struct DwtSystick<const FREQ: u32> {
     dwt: DWT,
     systick: SYST,
-    _mhz: PhantomData<MHZ>,
-    _khz: PhantomData<KHZ>,
-    _hz: PhantomData<HZ>,
 }
 
-impl<MHZ, KHZ, HZ> DwtSystick<MHZ, KHZ, HZ>
-where
-    MHZ: Unsigned,
-    KHZ: Unsigned,
-    HZ: Unsigned,
-{
+impl<const FREQ: u32> DwtSystick<FREQ> {
     /// Enable the DWT and provide a new `Monotonic` based on DWT and SysTick.
     ///
     /// Note that the `sysclk` parameter should come from e.g. the HAL's clock generation function
     /// so the real speed and the declared speed can be compared.
     pub fn new(dcb: &mut DCB, dwt: DWT, systick: SYST, sysclk: u32) -> Self {
-        assert!(MHZ::U32 * 1_000_000 + KHZ::U32 * 1_000 + HZ::U32 == sysclk);
-        assert!(KHZ::U32 < 1000);
-        assert!(HZ::U32 < 1000);
+        assert!(FREQ == sysclk);
 
         dcb.enable_trace();
         DWT::unlock();
@@ -46,26 +31,14 @@ where
 
         // We do not start the counter here, it is started in `reset`.
 
-        DwtSystick {
-            dwt,
-            systick,
-            _mhz: PhantomData,
-            _khz: PhantomData,
-            _hz: PhantomData,
-        }
+        DwtSystick { dwt, systick }
     }
 }
 
-impl<MHZ, KHZ, HZ> Clock for DwtSystick<MHZ, KHZ, HZ>
-where
-    MHZ: Unsigned,
-    KHZ: Unsigned,
-    HZ: Unsigned,
-{
+impl<const FREQ: u32> Clock for DwtSystick<FREQ> {
     type T = u32;
 
-    const SCALING_FACTOR: Fraction =
-        Fraction::new(1, MHZ::U32 * 1_000_000 + KHZ::U32 * 1_000 + HZ::U32);
+    const SCALING_FACTOR: Fraction = Fraction::new(1, FREQ);
 
     fn try_now(&self) -> Result<Instant<Self>, Error> {
         // The instant is always valid when the DWT is not reset
@@ -73,12 +46,7 @@ where
     }
 }
 
-impl<MHZ, KHZ, HZ> Monotonic for DwtSystick<MHZ, KHZ, HZ>
-where
-    MHZ: Unsigned,
-    KHZ: Unsigned,
-    HZ: Unsigned,
-{
+impl<const FREQ: u32> Monotonic for DwtSystick<FREQ> {
     const DISABLE_INTERRUPT_ON_EMPTY_QUEUE: bool = true;
 
     unsafe fn reset(&mut self) {
